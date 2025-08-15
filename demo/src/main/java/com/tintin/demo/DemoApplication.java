63b9b33b33912d5c639a2facafe645bc73272ec7
Tintin Andersson <tintin.andersson@sectra.com>
2025-08-15 11:38:25 +0200

    add swagger-ui

--- demo/src/main/java/com/tintin/demo/DemoApplication.java
+++ demo/src/main/java/com/tintin/demo/DemoApplication.java
@@ -2,14 +2,26 @@ package com.tintin.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+
+@OpenAPIDefinition(
+	info = @Info(
+		title = "Demo Employee API",
+		version = "1.0",
+		description = "API documentation for employee management"
+	)
+)
 @RestController
+@RequestMapping("/")
 @SpringBootApplication
 public class DemoApplication {
-
-	@RequestMapping("/")
+	@GetMapping
 	String home(){
 		return "Demo Employee API";
 	}
